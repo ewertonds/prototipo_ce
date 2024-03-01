5d37d386e71fd7381bff429d5356d3ab91b48354
Ewerton Ds <ewerton.eweds@gmail.com>
2024-02-29 21:17:26 -0300

    db_config update

--- db_config.php
+++ db_config.php
@@ -1,5 +1,5 @@
 <?php
-define("HOST", "localhost:3306");
+/*define("HOST", "localhost:3306");
 define("USUARIO", "ewertonds");
 define("SENHA", "NearedStow2");
 define("BANCO", "id21913578_bd_prototipo");
@@ -7,6 +7,19 @@ define("BANCO", "id21913578_bd_prototipo");
 $conn = new mysqli(HOST, USUARIO, SENHA, BANCO);
 if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
+}*/
+$host = "localhost:3306";
+$usuario = "ewertonds";
+$senha = "NearedStow2";
+$banco = "id21913578_bd_prototipo";
+
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
 }
 $conn->set_charset("utf8");
 ?>
+
+
